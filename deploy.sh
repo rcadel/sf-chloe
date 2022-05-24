6cb9f7966fa2df673f997fec57aa1197c09007a0
rcadel <renaud.cadel@weenat.com>
2022-05-24 15:09:27 +0200

    add script to deploy to s3

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf deploy
+mkdir deploy
+cp -r src/* deploy
+rm -rf deploy/input.css
+for f in deploy/*.html; do
+#This line splits the file name on the delimiter "."
+baseName=`echo $f | cut -d "." -f 1`
+mv $f $baseName
+done
+aws s3 sync deploy/ s3://$SF_STJO --exclude "*.*" --content-type "text/html" --profile perso
+aws s3 sync deploy/ s3://$SF_STJO --include "*.*" --profile perso
