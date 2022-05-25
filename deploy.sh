f1a1699ff11b02e13605c73f7ecf69fd7bbb7a02
rcadel <renaud.cadel@weenat.com>
2022-05-25 16:17:00 +0200

    invalidate on publish

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,4 @@ mv $f $baseName
 done
 aws s3 sync deploy/ s3://$SF_STJO --exclude "*.*" --content-type "text/html" --profile perso
 aws s3 sync deploy/ s3://$SF_STJO --include "*.*" --profile perso
+aws cloudfront create-invalidation --distribution-id $SF_STJO_DISTRIBUTION_ID --paths "/*" --profile perso
